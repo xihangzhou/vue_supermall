be962e18fd87bbfaae4bb3a776252605d95a7530
Xihang Zhou <47622010+xihangzhou@users.noreply.github.com>
2020-10-25 20:07:00 +0800

    NavBar-finished

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="navBar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="mid">
+      <slot name="mid"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'NavBar',
+};
+</script>
+
+<style>
+.navBar{
+    display: flex;
+    height: 44px;
+    text-align: center;
+    line-height: 44px;
+    box-shadow: 0 1px 1px rgba(100,100,100,.1);
+}
+
+.left,.right{
+    width: 60px;
+}
+
+.mid{
+    flex:auto;
+}
+
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class='home-nav'>
+      <template v-slot:mid>
+        <div>购物街</div>
+      </template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar.vue";
 export default {
-    name:'home'
-}
+  name: "home",
+  components: {
+    NavBar,
+  },
+};
 </script>
 
 <style>
-
+.home-nav{
+    background-color: pink;
+    color: white;
+}
 </style>
